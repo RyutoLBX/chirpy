b4c3907582bcbfa4b283442a2efa9bb7007de6e2
RyutoLBX <37038835+RyutoLBX@users.noreply.github.com>
2025-08-09 22:02:45 +0700

    feat: add request count middleware and monitoring endpoints
    - add request count middleware for /app
    - add /healthz, /metrics, and /reset handlers
    - register new handlers in server mux

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{}
+
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.getRequestCountHandler)
+	mux.HandleFunc("/reset", apiCfg.resetRequestCountHandler)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+func readinessHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (apiCfg *apiConfig) getRequestCountHandler(w http.ResponseWriter, req *http.Request) {
+	numRequests := apiCfg.fileserverHits.Load()
+	responseBody := fmt.Sprintf("Hits: %d", numRequests)
+
+	w.WriteHeader(200)
+	w.Write([]byte(responseBody))
+}
+
+func (apiCfg *apiConfig) resetRequestCountHandler(w http.ResponseWriter, req *http.Request) {
+	apiCfg.fileserverHits.Store(0)
+
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
